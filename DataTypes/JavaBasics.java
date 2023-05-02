f95af0e1323c1e2a2752f382c999c5ff59eb069e
Rohini Kulkarni <kulkarnirohini879@gmail.comgit config --global user.emailgit config --global user.email kulkarnirohini879@gmail.com>
2023-05-02 22:38:57 +0530

    basics
    practice
    
    practice

--- DataTypes/JavaBasics.java
+++ DataTypes/JavaBasics.java
@@ -1,6 +1,42 @@
-public class JavaBasics {
-   public static void main(String[] args) {
-        System.out.print("hello");
+// public class JavaBasics {
+//    public static void main(String[] args) {
+//         System.out.print("hello");
 
-    }
+//     }
+// }
+// sum of a and b (input from user)
+
+// import java.util.*;
+
+// public class JavaBasics {
+//    public static void main(String[] args) {
+//      Scanner sc = new Scanner(System.in);
+//      int a = sc.nextInt();
+//      int b = sc.nextInt();
+//      int sum = a + b ;
+//      System.out.println(sum);
+
+//    }
+// }
+
+// import java.util.*;
+// public class JavaBasics{
+//   public static void main(String[] args){
+//     Scanner sc = new Scanner (System.in);
+//    float r = sc.nextFloat();
+//    float area = 3.14f*r*r;
+//     System.out.print(area);
+//   }
+// }
+
+import java.util.*;
+public class JavaBasics{
+  public static void main (String[]args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int product= a*b;
+    System.out.print(product);
+
+  }
 }
