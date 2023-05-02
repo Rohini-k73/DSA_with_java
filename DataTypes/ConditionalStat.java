a45207026001608cf045a3732156e2c9751873af
Rohini Kulkarni <kulkarnirohini879@gmail.comgit config --global user.emailgit config --global user.email kulkarnirohini879@gmail.com>
2023-05-02 23:51:32 +0530

    practice
    practice
    
    hehehe

---
+++ DataTypes/ConditionalStat.java
@@ -0,0 +1,66 @@
+// if else
+// print the largest of two numbers
+
+// import java .util.*;
+
+// public class ConditionalStat{
+//     public static void main(String []args){
+//         int a = 2;
+//         int b = 8;
+//         if(a>=b){
+//             System.out.print("a is large");
+//         }else{
+//             System.out.print("b is large");
+//         }
+    
+//     }
+// }
+
+// ?print nu is odd or even 
+
+// import java .util.*;
+
+// public class ConditionalStat{
+//     public static void main(String []args){
+
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+
+//         if (num%2==0){
+//             System.out.print("even");
+//         }else
+//         System.out.print("odd");
+//     }
+//     }
+
+// import java .util.*;
+
+// public class ConditionalStat{
+//     public static void main(String []args){
+
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+
+//         if (num%2==1){
+//             System.out.print("odd");
+//         }else
+//         System.out.print("even");
+//     }
+//     }
+
+// print largest of 3 num 
+
+import java .util.*;
+
+public class ConditionalStat{
+    public static void main(String []args){
+
+      int a = 2, b = 6, c = 9;
+
+      if((a>=b) && (a>=c)){
+        System.out.println("a is larger");
+      }else if (b>=c){
+        System.out.println("b is larger");
+      } 
+
+    }}
