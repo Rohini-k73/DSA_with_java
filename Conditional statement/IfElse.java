ce4fcfdf2c7dfe6444e347b2878f21228dbbb26d
Rohini Kulkarni <kulkarnirohini879@gmail.comgit config --global user.emailgit config --global user.email kulkarnirohini879@gmail.com>
2023-05-03 00:00:05 +0530

    ifelse
    practice
    
    try

---
+++ Conditional statement/IfElse.java
@@ -0,0 +1,14 @@
+// ternary operators
+
+//  even and odd 
+
+import java.util.*;
+
+public class IfElse{
+    public static void main (String []args){
+int num = 4;
+String type = ((num%2 == 0)) ? "even" : "odd";
+System.out.println (type);
+
+    }
+}
