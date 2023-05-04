e1d4925e8e6b0371e2ec3c69f38d3d88edfe2c63
Rohini Kulkarni <kulkarnirohini879@gmail.comgit config --global user.emailgit config --global user.email kulkarnirohini879@gmail.com>
2023-05-05 01:07:35 +0530

    finish loop
    todays done

--- DataTypes/JavaBasics.java
+++ DataTypes/JavaBasics.java
@@ -1,33 +1,33 @@
-// public class JavaBasics {
-//    public static void main(String[] args) {
-//         System.out.print("hello");
+public class JavaBasics {
+   public static void main(String[] args) {
+        System.out.print("hello");
 
-//     }
-// }
-// sum of a and b (input from user)
+    }
+}
+sum of a and b (input from user)
 
-// import java.util.*;
+import java.util.*;
 
-// public class JavaBasics {
-//    public static void main(String[] args) {
-//      Scanner sc = new Scanner(System.in);
-//      int a = sc.nextInt();
-//      int b = sc.nextInt();
-//      int sum = a + b ;
-//      System.out.println(sum);
+public class JavaBasics {
+   public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     int a = sc.nextInt();
+     int b = sc.nextInt();
+     int sum = a + b ;
+     System.out.println(sum);
 
-//    }
-// }
+   }
+}
 
-// import java.util.*;
-// public class JavaBasics{
-//   public static void main(String[] args){
-//     Scanner sc = new Scanner (System.in);
-//    float r = sc.nextFloat();
-//    float area = 3.14f*r*r;
-//     System.out.print(area);
-//   }
-// }
+import java.util.*;
+public class JavaBasics{
+  public static void main(String[] args){
+    Scanner sc = new Scanner (System.in);
+   float r = sc.nextFloat();
+   float area = 3.14f*r*r;
+    System.out.print(area);
+  }
+}
 
 import java.util.*;
 public class JavaBasics{
